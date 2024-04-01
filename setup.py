9b573b27590db1cbd6c8ef08fca7ff8c9d26b340
callummcdougall <cal.s.mcdougall@gmail.com>
2024-04-01 11:11:45 +0100

    fix hook point bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'sae-vis',
-    version = '0.2.4',
+    version = '0.2.5',
     packages = find_packages(),
     install_requires = [
         'torch',
