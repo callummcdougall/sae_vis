2fbae4c9a7dd663737bae25e73e978d40c59064a
callummcdougall <cal.s.mcdougall@gmail.com>
2024-04-05 12:44:57 +0100

    re-fix html anomalies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'sae-vis',
-    version = '0.2.5',
+    version = '0.2.6',
     packages = find_packages(),
     install_requires = [
         'torch',
