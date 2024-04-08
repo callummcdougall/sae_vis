72e584b6492ed1ef3989968f6588a17fca758650
callummcdougall <cal.s.mcdougall@gmail.com>
2024-04-08 09:22:22 +0100

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name = 'sae-vis',
-    version = '0.2.11',
+    version = '0.2.12',
     packages = find_packages(),
     install_requires = [
         'torch',
